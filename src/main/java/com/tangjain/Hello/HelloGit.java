da01691a458ab6c5c490ea3436dcc606ce78b5ee
8000 <602060591@qq.com>
2021-03-11 09:59:27 +0800

    Commit Message

---
+++ src/main/java/com/tangjain/Hello/HelloGit.java
@@ -0,0 +1,14 @@
+package com.tangjain.Hello;
+
+/**
+ * @Author 60206
+ * @Create 2021/3/11 - 9:50
+ * @Description
+ */
+public class HelloGit {
+
+    public void show(){
+        System.out.println("HelloGit");
+    }
+
+}
---
+++ src/test/java/com/tangjian/test/GitTest.java
@@ -0,0 +1,20 @@
+package com.tangjian.test;
+
+import com.tangjain.Hello.HelloGit;
+import org.junit.Test;
+
+/**
+ * @Author 60206
+ * @Create 2021/3/11 - 9:51
+ * @Description
+ */
+public class GitTest {
+
+
+    @Test
+    public void test(){
+        HelloGit helloGit = new HelloGit();
+        helloGit.show();
+    }
+
+}
